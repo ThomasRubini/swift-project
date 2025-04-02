6fba9ac408fb52a3234960781586770b81903a4a
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-04-02 09:37:54 +0200

    add testUpdateTaskState()

--- Tests/swift-projectTests/TaskLinkedListTests.swift
+++ Tests/swift-projectTests/TaskLinkedListTests.swift
@@ -21,4 +21,23 @@ struct TaskLinkedListTests {
         #expect(linkedList.len() == 1, "List should contain one task after adding.")
         #expect(linkedList.get(0) == task, "First task should be the one added.")
     }
+
+    @Test
+    func testUpdateTaskState() {
+        let linkedList = TaskLinkedList()
+
+        let task = Task(
+            title: "Test task",
+            description: "Test description",
+            priority: 5,
+            requiredRAM: 256,
+            estimatedTime: 120,
+            state: .todo
+        )
+
+        linkedList.add(task)
+
+        #expect(linkedList.updateTaskState(by: "Test task", newState: .inProgress), "Task state should be updated.")
+        #expect(linkedList.get(0)?.state == .inProgress, "Task state should be inProgress.")
+    }
 }
