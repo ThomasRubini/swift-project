ea21d0549f65bc6e0ea72a4747f4c21596b9bed4
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-04-02 09:01:43 +0200

    rename more files

--- Sources/TaskLinkedList.swift
+++ Sources/TaskLinkedList.swift
@@ -1,33 +1,3 @@
-import Foundation
-
-enum TaskState: String {
-    case todo = "à faire"
-    case inProgress = "en cours"
-    case completed = "terminée"
-}
-
-struct Task : Equatable {
-    var title: String              
-    var description: String        
-    var priority: Int              
-    var requiredRAM: Int           
-    var estimatedTime: Double       
-    var state: TaskState             
-
-    init(title: String, description: String, priority: Int, requiredRAM: Int, estimatedTime: Double, state: TaskState = .todo) {
-        self.title = title
-        self.description = description
-        self.priority = priority
-        self.requiredRAM = requiredRAM
-        self.estimatedTime = estimatedTime
-        self.state = state
-    }
-    
-    static func == (lhs: Task, rhs: Task) -> Bool {
-        lhs.title == rhs.title
-    }
-}
-
 class TaskList: LinkedList<Task> {
     func updateTaskState(by title: String, newState: TaskState) -> Bool {
         var currentNode = self.head
